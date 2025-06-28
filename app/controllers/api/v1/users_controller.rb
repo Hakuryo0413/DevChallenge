bc81125ecb373a983c7d167acee7918976fe9789
Hakuryo0413 <21020413@vnu.edu.vn>
2025-06-29 02:21:34 +0700

    Update show def user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :set_user, only: %i[ show ]
+
       def index
         top_users = User.order(total_points: :desc).limit(10)
         render json: {
@@ -9,6 +11,21 @@ module Api
           data: top_users.map { |user| user.as_json(only: [ :id, :email, :total_points ]) }
         }, status: :ok
       end
+
+      def show
+        if @user
+          render json: { message: "User found", data: @user }
+        else
+          render json: { message: "User not found", data: @user.errors }
+        end
+      end
+
+
+      private
+        def set_user
+          @user = User.find(params[:id])
+        end
+
     end
   end
 end
