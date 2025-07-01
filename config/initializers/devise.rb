a79d64c8637c5610de7032defd1af4bb64f667a8
Hakuryo0413 <21020413@vnu.edu.vn>
2025-07-01 19:39:42 +0700

    Add docker-compose and update Dockerfile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -306,7 +306,7 @@ Devise.setup do |config|
   config.responder.redirect_status = :see_other
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.secret = ENV.fetch("SECRET_KEY_BASE")
     jwt.dispatch_requests = [
       [ "POST", %r{^/login$} ]
     ]
