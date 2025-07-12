4c1836ee3216551eb50f8444f9a596e332edfbd2
Hakuryo0413 <21020413@vnu.edu.vn>
2025-07-13 00:14:57 +0700

    Fix lint code

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -9,15 +9,14 @@ class Submission < ApplicationRecord
   def add_points_if_accepted
     puts "CALLBACK TRIGGERED"
     puts "Status: #{status}"
-  
+
     return unless status == "Accepted"
-  
+
     puts "Status is Accepted, proceeding..."
     puts "User: #{user.inspect}"
     puts "Question: #{question.inspect}"
     puts "Points: #{question.points.inspect}"
-  
+
     user.increment!(:total_points, question.points.to_i)
   end
-  
 end
